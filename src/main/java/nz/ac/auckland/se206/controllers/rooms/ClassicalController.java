e5a1914bfbaac63cfc12621682bea612994ed63b
alux444 <aluux999@gmail.com>
2023-08-30 01:06:30 +1200

    feat - lo-fi door implementation

--- src/main/java/nz/ac/auckland/se206/controllers/rooms/ClassicalController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/rooms/ClassicalController.java
@@ -1,8 +1,32 @@
 package nz.ac.auckland.se206.controllers.rooms;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class ClassicalController {
+
+  @FXML private Rectangle raveDoor;
+  @FXML private Rectangle rockDoor;
+
   @FXML
   private void initialize() {}
+
+  @FXML
+  private void doGoRave(MouseEvent event) throws IOException {
+    Rectangle current = (Rectangle) event.getSource();
+    Scene currentScene = current.getScene();
+    currentScene.setRoot(SceneManager.getUiRoot(AppUi.RAVE));
+  }
+
+  @FXML
+  private void doGoRock(MouseEvent event) throws IOException {
+    Rectangle current = (Rectangle) event.getSource();
+    Scene currentScene = current.getScene();
+    currentScene.setRoot(SceneManager.getUiRoot(AppUi.ROCK));
+  }
 }
--- src/main/java/nz/ac/auckland/se206/controllers/rooms/RockController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/rooms/RockController.java
@@ -1,8 +1,31 @@
 package nz.ac.auckland.se206.controllers.rooms;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class RockController {
+  @FXML private Rectangle classicalDoor;
+  @FXML private Rectangle raveDoor;
+
   @FXML
   private void initialize() {}
+
+  @FXML
+  private void doGoRave(MouseEvent event) throws IOException {
+    Rectangle current = (Rectangle) event.getSource();
+    Scene currentScene = current.getScene();
+    currentScene.setRoot(SceneManager.getUiRoot(AppUi.RAVE));
+  }
+
+  @FXML
+  private void doGoClassical(MouseEvent event) throws IOException {
+    Rectangle current = (Rectangle) event.getSource();
+    Scene currentScene = current.getScene();
+    currentScene.setRoot(SceneManager.getUiRoot(AppUi.CLASSICAL));
+  }
 }
