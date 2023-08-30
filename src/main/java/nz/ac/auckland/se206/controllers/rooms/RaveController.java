e1c56d126b9e2523f6c1b6e3fdbfb4cca4e88216
alux444 <aluux999@gmail.com>
2023-08-30 18:05:48 +1200

    feat - Basic rave room setup

--- src/main/java/nz/ac/auckland/se206/controllers/rooms/RaveController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/rooms/RaveController.java
@@ -3,6 +3,7 @@ package nz.ac.auckland.se206.controllers.rooms;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.se206.SceneManager;
@@ -11,9 +12,45 @@ import nz.ac.auckland.se206.SceneManager.AppUi;
 public class RaveController {
   @FXML private Rectangle classicalDoor;
   @FXML private Rectangle rockDoor;
+  @FXML private ImageView posterBtn;
+  @FXML private ImageView djBtn;
+  @FXML private ImageView bodybuilderBtn;
+  @FXML private ImageView bouncerBtn;
+  @FXML private ImageView discoBtn;
+  @FXML private ImageView speakerBtn;
 
   @FXML
   private void initialize() {}
+
+  @FXML
+  private void onClickPoster(MouseEvent event) {
+    System.out.println("poster clicked");
+  }
+
+  @FXML
+  private void onClickDj(MouseEvent event) {
+    System.out.println("dj clicked");
+  }
+
+  @FXML
+  private void onClickBodybuilder(MouseEvent event) {
+    System.out.println("bodybuilder clicked");
+  }
+
+  @FXML
+  private void onClickBouncer(MouseEvent event) {
+    System.out.println("bouncer clicked");
+  }
+
+  @FXML
+  private void onClickDisco(MouseEvent event) {
+    System.out.println("disco clicked");
+  }
+
+  @FXML
+  private void onClickSpeaker(MouseEvent event) {
+    System.out.println("speaker clicked");
+  }
 
   @FXML
   private void doGoClassical(MouseEvent event) throws IOException {
