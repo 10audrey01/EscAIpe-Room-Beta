7d7ca6cdffd21dd953b4ebc9d26ec4ef543bc996
alux444 <aluux999@gmail.com>
2023-08-30 18:23:15 +1200

    feat - Added indicator logic for rave room

--- src/main/java/nz/ac/auckland/se206/controllers/rooms/RaveController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/rooms/RaveController.java
@@ -46,6 +46,105 @@ public class RaveController {
   }
 
   @FXML
+  private void onDjEnter() throws IOException {
+    Image currentImage =
+        new Image(App.class.getResource("/images/raveRoom/djSel.png").openStream());
+    Platform.runLater(
+        () -> {
+          djBtn.setImage(currentImage);
+        });
+  }
+
+  @FXML
+  private void onDjExit() throws IOException {
+    Image currentImage = new Image(App.class.getResource("/images/raveRoom/dj.png").openStream());
+    Platform.runLater(
+        () -> {
+          djBtn.setImage(currentImage);
+        });
+  }
+
+  @FXML
+  private void onSpeakerEnter() throws IOException {
+    Image currentImage =
+        new Image(App.class.getResource("/images/raveRoom/speakerSel.png").openStream());
+    Platform.runLater(
+        () -> {
+          speakerBtn.setImage(currentImage);
+        });
+  }
+
+  @FXML
+  private void onSpeakerExit() throws IOException {
+    Image currentImage =
+        new Image(App.class.getResource("/images/raveRoom/speaker.png").openStream());
+    Platform.runLater(
+        () -> {
+          speakerBtn.setImage(currentImage);
+        });
+  }
+
+  @FXML
+  private void onBodybuilderEnter() throws IOException {
+    Image currentImage =
+        new Image(App.class.getResource("/images/raveRoom/bodybuilderSel.png").openStream());
+    Platform.runLater(
+        () -> {
+          bodybuilderBtn.setImage(currentImage);
+        });
+  }
+
+  @FXML
+  private void onBodybuilderExit() throws IOException {
+    Image currentImage =
+        new Image(App.class.getResource("/images/raveRoom/bodybuilder.png").openStream());
+    Platform.runLater(
+        () -> {
+          bodybuilderBtn.setImage(currentImage);
+        });
+  }
+
+  @FXML
+  private void onDiscoEnter() throws IOException {
+    Image currentImage =
+        new Image(App.class.getResource("/images/raveRoom/discoballSel.png").openStream());
+    Platform.runLater(
+        () -> {
+          discoBtn.setImage(currentImage);
+        });
+  }
+
+  @FXML
+  private void onDiscoExit() throws IOException {
+    Image currentImage =
+        new Image(App.class.getResource("/images/raveRoom/discoball.png").openStream());
+    Platform.runLater(
+        () -> {
+          discoBtn.setImage(currentImage);
+        });
+  }
+
+  @FXML
+  private void onBouncerEnter() throws IOException {
+    Image currentImage =
+        new Image(App.class.getResource("/images/raveRoom/bouncerSel.png").openStream());
+    Platform.runLater(
+        () -> {
+          bouncerBtn.setImage(currentImage);
+        });
+  }
+
+  @FXML
+  private void onBouncerExit() throws IOException {
+    Image currentImage =
+        new Image(App.class.getResource("/images/raveRoom/bouncer.png").openStream());
+    Platform.runLater(
+        () -> {
+          bouncerBtn.setImage(currentImage);
+        });
+  }
+
+  @FXML
   private void onClickPoster(MouseEvent event) {
     System.out.println("poster clicked");
   }
