4d2bf1f1ac4c57fd241dde9512bb271895c96e28
alux444 <aluux999@gmail.com>
2023-09-06 17:20:31 +1200

    feat - Start Menu screen

--- src/main/java/nz/ac/auckland/se206/controllers/StartController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartController.java
@@ -1,8 +1,16 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import nz.ac.auckland.se206.GameState;
+import nz.ac.auckland.se206.GameState.Difficulty;
+import nz.ac.auckland.se206.GameState.PlayTime;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class StartController {
 
@@ -14,8 +22,13 @@ public class StartController {
   @FXML private RadioButton sixMinsBtn;
   @FXML private Button startGameBtn;
 
+  private GameState gamestate = GameState.getInstance();
+
   @FXML
-  private void initialize() {}
+  private void initialize() {
+    onClickMedium();
+    onClickFour();
+  }
 
   private void resetDifficulty() {
     easyBtn.selectedProperty().set(false);
@@ -33,38 +46,54 @@ public class StartController {
   private void onClickEasy() {
     resetDifficulty();
     easyBtn.selectedProperty().set(true);
+    gamestate.setDifficulty(Difficulty.EASY);
   }
 
   @FXML
   private void onClickMedium() {
     resetDifficulty();
     mediumBtn.selectedProperty().set(true);
+    gamestate.setDifficulty(Difficulty.MEDIUM);
   }
 
   @FXML
   private void onClickHard() {
     resetDifficulty();
     hardBtn.selectedProperty().set(true);
+    gamestate.setDifficulty(Difficulty.HARD);
   }
 
   @FXML
   private void onClickTwo() {
     resetTime();
     twoMinsBtn.selectedProperty().set(true);
+    gamestate.setTime(PlayTime.TWO);
   }
 
   @FXML
   private void onClickFour() {
     resetTime();
     fourMinsBtn.selectedProperty().set(true);
+    gamestate.setTime(PlayTime.FOUR);
   }
 
   @FXML
   private void onClickSix() {
     resetTime();
     sixMinsBtn.selectedProperty().set(true);
+    gamestate.setTime(PlayTime.SIX);
   }
 
   @FXML
-  private void onGameStart() {}
+  private void onGameStart(ActionEvent event) throws IOException {
+    Button current = (Button) event.getSource();
+    Scene currentScene = current.getScene();
+    currentScene.setRoot(SceneManager.getUiRoot(AppUi.ROCK));
+    System.out.println(
+        "Started with difficulty: "
+            + gamestate.getDifficulty()
+            + " and time: "
+            + gamestate.getTime()
+            + " minutes.");
+  }
 }
