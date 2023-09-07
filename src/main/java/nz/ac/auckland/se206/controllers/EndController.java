81bdba2a62f1edbef154ddbbe853352ad8ec0388
audrey <ayou332@aucklanduni.ac.nz>
2023-09-07 20:28:33 +1200

    feat - Display text of win/lose when time runs out, and also functionality of exit button

--- src/main/java/nz/ac/auckland/se206/controllers/EndController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/EndController.java
@@ -2,11 +2,14 @@ package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
 import nz.ac.auckland.se206.controllers.rooms.ClassicalController;
 import nz.ac.auckland.se206.controllers.rooms.RaveController;
 import nz.ac.auckland.se206.controllers.rooms.RockController;
@@ -34,8 +37,14 @@ public class EndController {
   }
 
   @FXML
-  private void onBack() throws IOException {
-    App.setRoot("start");
+  private void onBack(ActionEvent event) throws IOException {
+    GameState.isRiddleResolved = false;
+    GameState.isKeyFound = false;
+    GameState.isEscaped = false;
+
+    Button current = (Button) event.getSource();
+    Scene currentScene = current.getScene();
+    currentScene.setRoot(SceneManager.getUiRoot(AppUi.START));
   }
 
   @FXML
