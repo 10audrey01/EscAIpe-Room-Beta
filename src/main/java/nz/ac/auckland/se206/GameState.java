de8506aed11ed28acec05d4922de3e03cdb09cd9
audrey <ayou332@aucklanduni.ac.nz>
2023-09-07 17:06:15 +1200

    feat - PlayTime enum elements now have an integer associated with it

--- src/main/java/nz/ac/auckland/se206/GameState.java
+++ src/main/java/nz/ac/auckland/se206/GameState.java
@@ -9,9 +9,19 @@ public class GameState {
   }
 
   public enum PlayTime {
-    TWO,
-    FOUR,
-    SIX
+    TWO(2),
+    FOUR(4),
+    SIX(6);
+
+    private final int time;
+
+    PlayTime(int time) {
+      this.time = time;
+    }
+
+    public int getTime() {
+      return this.time;
+    }
   }
 
   // variables to keep track of the game state
