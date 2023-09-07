bf70378b77da0cfd3fb59e82c45fa2745926832a
Liyork Liao <liyorkl@gmail.com>
2023-09-08 02:33:11 +1200

    feat Added list for TextAreas and textFields This allows for us to change the controls whilst not in the same scene

--- src/main/java/nz/ac/auckland/se206/ChatManager.java
+++ src/main/java/nz/ac/auckland/se206/ChatManager.java
@@ -1,6 +1,11 @@
 package nz.ac.auckland.se206;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import nz.ac.auckland.se206.gpt.ChatMessage;
 import nz.ac.auckland.se206.gpt.GptPromptEngineering;
 import nz.ac.auckland.se206.gpt.openai.ApiProxyException;
@@ -11,10 +16,31 @@ import nz.ac.auckland.se206.gpt.openai.ChatCompletionResult.Choice;
 public class ChatManager {
 
   private ChatCompletionRequest chatCompletionRequest;
-  private ArrayList<String> messages;
+  private ArrayList<TextArea> TextAreas;
+  private ArrayList<TextField> TextFields;
+  private String messages;
 
   public ChatManager() {
-    this.messages = new ArrayList<String>();
+    this.messages = "";
+    TextAreas = new ArrayList<TextArea>();
+    TextFields = new ArrayList<TextField>();
+  }
+
+  public void addTextArea(TextArea textArea) {
+    TextAreas.add(textArea);
+  }
+
+  public void addTextField(TextField textField) {
+    TextFields.add(textField);
+  }
+
+  public void clearAllTextFields() {
+    Platform.runLater(
+        () -> {
+          for (TextField textField : TextFields) {
+            textField.setText("");
+          }
+        });
   }
 
   public void generateInitialMessage() throws ApiProxyException {
@@ -24,7 +50,26 @@ public class ChatManager {
   }
 
   public void addMessage(ChatMessage msg) {
-    messages.add(msg.getRole() + ": " + msg.getContent() + "\n\n");
+    messages = messages.concat(msg.getRole() + ": " + msg.getContent() + "\n\n");
+    Platform.runLater(
+        () -> {
+          for (TextArea textArea : TextAreas) {
+            textArea.setText(messages);
+          }
+        });
+  }
+
+  @FXML
+  public void onSendMessage(TextField inputText) throws ApiProxyException, IOException {
+    String message = inputText.getText();
+    System.out.println(message);
+    if (message.trim().isEmpty()) {
+      System.out.println("Whoops");
+      return;
+    }
+    clearAllTextFields();
+    ChatMessage msg = new ChatMessage("user", message);
+    addMessage(msg);
   }
 
   /**
