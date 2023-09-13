93780632451e252eecb0e456952865ce70dfb038
audrey <ayou332@aucklanduni.ac.nz>
2023-09-13 15:59:42 +1200

    refactor - changed notes array type from string to enum of type Note

--- src/main/java/nz/ac/auckland/se206/RockBigTaskManager.java
+++ src/main/java/nz/ac/auckland/se206/RockBigTaskManager.java
@@ -2,9 +2,20 @@ package nz.ac.auckland.se206;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import javafx.scene.control.Label;
 
 public class RockBigTaskManager {
+  public enum Note {
+    C,
+    D,
+    E,
+    F,
+    G,
+    A,
+    B;
+  }
+
   public enum Colour {
     YELLOW("#EADE3B"),
     BLUE("#2E2EB6"),
@@ -22,17 +33,18 @@ public class RockBigTaskManager {
     }
   }
 
-  private String[] noteSequence;
+  private Note[] noteSequence;
   private ArrayList<Label> colourLabels1;
   private ArrayList<Label> colourLabels2;
   private ArrayList<Label> colourLabels3;
   private ArrayList<Label> colourLabels4;
   private ArrayList<Colour> colours;
-  private ArrayList<String> notes;
+  private ArrayList<Note> notes;
   private ArrayList<Integer> order;
+  private HashMap<Colour, Integer> orderColourMap;
 
   RockBigTaskManager() {
-    this.noteSequence = new String[4];
+    this.noteSequence = new Note[4];
     this.colourLabels1 = new ArrayList<Label>();
     this.colourLabels2 = new ArrayList<Label>();
     this.colourLabels3 = new ArrayList<Label>();
@@ -43,14 +55,14 @@ public class RockBigTaskManager {
     colours.add(Colour.PURPLE);
     colours.add(Colour.CYAN);
     Collections.shuffle(colours);
-    this.notes = new ArrayList<String>();
-    notes.add("C");
-    notes.add("D");
-    notes.add("E");
-    notes.add("F");
-    notes.add("G");
-    notes.add("A");
-    notes.add("B");
+    this.notes = new ArrayList<Note>();
+    notes.add(Note.C);
+    notes.add(Note.D);
+    notes.add(Note.E);
+    notes.add(Note.F);
+    notes.add(Note.G);
+    notes.add(Note.A);
+    notes.add(Note.B);
     setNoteSequence();
     this.order = new ArrayList<Integer>();
     order.add(1);
@@ -120,5 +132,21 @@ public class RockBigTaskManager {
     }
     System.out.println(
         "Note sequence: " + noteSequence[0] + noteSequence[1] + noteSequence[2] + noteSequence[3]);
+  }
+
+  public Note[] getNoteSequence() {
+    return noteSequence;
+  }
+
+  public void setOrderColourMap() {
+    orderColourMap = new HashMap<Colour, Integer>();
+    orderColourMap.put(colours.get(0), order.get(0));
+    orderColourMap.put(colours.get(1), order.get(1));
+    orderColourMap.put(colours.get(2), order.get(2));
+    orderColourMap.put(colours.get(3), order.get(3));
+  }
+
+  public HashMap<Colour, Integer> getOrderColourMap() {
+    return orderColourMap;
   }
 }
