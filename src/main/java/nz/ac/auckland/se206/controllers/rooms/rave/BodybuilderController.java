0ed1adda22ff8e13a14cfafef44bb4fa03987404
alux444 <aluux999@gmail.com>
2023-09-15 19:32:38 +1200

    feat - Added FXML items to controller for raveevent

--- src/main/java/nz/ac/auckland/se206/controllers/rooms/rave/BodybuilderController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/rooms/rave/BodybuilderController.java
@@ -1,3 +1,32 @@
 package nz.ac.auckland.se206.controllers.rooms.rave;
 
-public class BodybuilderController {}
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+
+public class BodybuilderController {
+  @FXML private TextArea speechBox;
+  @FXML private ImageView hintImage;
+  @FXML private Button btnOne;
+  @FXML private Button btnTwo;
+  @FXML private Button btnThree;
+  @FXML private Button btnFour;
+  @FXML private Button btnFive;
+  @FXML private Button btnSix;
+  @FXML private Button btnSeven;
+  @FXML private Button btnEight;
+  @FXML private Button btnNine;
+  @FXML private Button btnZero;
+  @FXML private Button btnRemove;
+  @FXML private Button btnSub;
+  @FXML private Button btnReturn;
+  @FXML private Text codeInputText;
+  @FXML private Circle digitOne;
+  @FXML private Circle digitTwo;
+  @FXML private Circle digitThree;
+  @FXML private Circle digitFour;
+  @FXML private Circle digitFive;
+  @FXML private Circle digitSix;
+}
