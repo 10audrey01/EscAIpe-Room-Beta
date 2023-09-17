62377723a9a3c19d81c5661f444d3c78fdf3b21e
alux444 <aluux999@gmail.com>
2023-09-17 18:05:15 +1200

    feat - added FXML and controller for music quiz event

---
+++ src/main/java/nz/ac/auckland/se206/controllers/rooms/rave/MusicQuizController.java
@@ -0,0 +1,32 @@
+package nz.ac.auckland.se206.controllers.rooms.rave;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import nz.ac.auckland.se206.GameState;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
+
+public class MusicQuizController {
+  @FXML private TextArea speechBox;
+  @FXML private Button btnReturn;
+
+  private GameState gamestate;
+
+  @FXML
+  private void initialize() throws IOException {
+    this.gamestate = GameState.getInstance();
+    this.speechBox.setText("Hey man, I need your help identifying this music...");
+  }
+
+  // switches back to the rave room
+  @FXML
+  private void onClickReturn(ActionEvent event) {
+    Button source = (Button) event.getSource();
+    Scene currentScene = source.getScene();
+    currentScene.setRoot(SceneManager.getUiRoot(AppUi.RAVE));
+  }
+}
