147bbfe063576af68b41e746447c1540c14c7b53
alux444 <aluux999@gmail.com>
2023-09-21 18:45:35 +1200

    docs - start controller comments

--- src/main/java/nz/ac/auckland/se206/controllers/StartController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartController.java
@@ -29,6 +29,7 @@ public class StartController {
 
   private GameState gameState;
 
+  // function for handling initialisation of the room
   @FXML
   private void initialize() {
     this.gameState = GameState.getInstance();
@@ -36,66 +37,79 @@ public class StartController {
 
   @FXML
   private void onClickEasy() {
+    // Set the game difficulty to EASY when the "Easy" button is clicked.
     GameState.difficulty = Difficulty.EASY;
   }
 
   @FXML
   private void onEasyEnter() {
+    // Display a hint for the EASY difficulty when the mouse enters the "Easy" button area.
     difficultyDescriptionLabel.setText("You can ask for as many hints as you want!");
   }
 
   @FXML
   private void onEasyExit() {
+    // Clear the hint when the mouse exits the "Easy" button area.
     difficultyDescriptionLabel.setText("");
   }
 
   @FXML
   private void onClickMedium() {
+    // Set the game difficulty to MEDIUM when the "Medium" button is clicked.
     GameState.difficulty = Difficulty.MEDIUM;
   }
 
   @FXML
   private void onMediumEnter() {
+    // Display a hint for the MEDIUM difficulty when the mouse enters the "Medium" button area.
     difficultyDescriptionLabel.setText("You can ask for a maximum of 5 hints!");
   }
 
   @FXML
   private void onMediumExit() {
+    // Clear the hint when the mouse exits the "Medium" button area.
     difficultyDescriptionLabel.setText("");
   }
 
   @FXML
   private void onClickHard() {
+    // Set the game difficulty to HARD when the "Hard" button is clicked.
     GameState.difficulty = Difficulty.HARD;
   }
 
   @FXML
   private void onHardEnter() {
+    // Display a hint for the HARD difficulty when the mouse enters the "Hard" button area.
     difficultyDescriptionLabel.setText("No hints will be given!");
   }
 
   @FXML
   private void onHardExit() {
+    // Clear the hint when the mouse exits the "Hard" button area.
     difficultyDescriptionLabel.setText("");
   }
 
   @FXML
   private void onClickTwo() {
+    // Set the game playtime to TWO minutes when the "2 Minutes" button is clicked.
     GameState.time = PlayTime.TWO;
   }
 
   @FXML
   private void onClickFour() {
+    // Set the game playtime to FOUR minutes when the "4 Minutes" button is clicked.
     GameState.time = PlayTime.FOUR;
   }
 
   @FXML
   private void onClickSix() {
+    // Set the game playtime to SIX minutes when the "6 Minutes" button is clicked.
     GameState.time = PlayTime.SIX;
   }
 
   @FXML
   private void onGameStart(ActionEvent event) throws IOException {
+    // guard clauses for checking if there are difficulties / times selected
     if (easyBtn.getToggleGroup().getSelectedToggle() == null) { // no difficulty selected
       difficultyDescriptionLabel.setText("Please select a difficulty!");
       return;
@@ -115,17 +129,18 @@ public class StartController {
     SceneManager.addController(AppUi.PIANO, pianoLoader.getController());
     SceneManager.addController(AppUi.HARP, harpLoader.getController());
 
+    // reloads all FXML files required for playing the game
     SceneManager.addUi(AppUi.CLASSICAL, App.loadFxml("classical"));
     SceneManager.addUi(AppUi.RAVE, App.loadFxml("rave"));
     SceneManager.addUi(AppUi.ROCK, App.loadFxml("rock"));
     SceneManager.addUi(AppUi.BODYBUILDER, App.loadFxml("bodybuilder"));
     SceneManager.addUi(AppUi.GUITARIST, App.loadFxml("guitarist"));
     SceneManager.addUi(AppUi.TRUMPET, App.loadFxml("trumpet"));
-
     SceneManager.addUi(AppUi.ROCKNOTE, App.loadFxml("rocknote"));
     SceneManager.addUi(AppUi.CLASSICALNOTE, App.loadFxml("classicalnote"));
     SceneManager.addUi(AppUi.MUSICQUIZ, App.loadFxml("musicquiz"));
 
+    // switches the current scene to the rock room
     Button current = (Button) event.getSource();
     Scene currentScene = current.getScene();
     currentScene.setRoot(SceneManager.getUiRoot(AppUi.ROCK));
@@ -135,6 +150,7 @@ public class StartController {
             + " and time: "
             + GameState.time
             + " minutes.");
+    // run the gamestate's start function
     gameState.startGame(); // including generating tasks
   }
 }
