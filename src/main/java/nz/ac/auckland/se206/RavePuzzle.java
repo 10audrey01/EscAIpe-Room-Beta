e02a0eee15b777913aad806c42305552c49774a0
alux444 <aluux999@gmail.com>
2023-10-05 19:50:00 +1300

    docs - javadocs for the rave puzzle class

--- src/main/java/nz/ac/auckland/se206/RavePuzzle.java
+++ src/main/java/nz/ac/auckland/se206/RavePuzzle.java
@@ -8,17 +8,19 @@ import javafx.scene.image.ImageView;
 import nz.ac.auckland.se206.controllers.rooms.notes.ClassicalNoteController;
 import nz.ac.auckland.se206.controllers.rooms.notes.RockNoteController;
 
+/**
+ * A class representing the Rave Puzzle, which involves finding a safe and unlocking its code by
+ * finding specific objects in both the Rock and Classical rooms.
+ */
 public class RavePuzzle {
-  // rave puzzle - Find the safe, unlock the code through finding respective object in each of rock
-  // and classical rooms
 
   private String safeSolution; // Stores the randomly generated safe code solution
   private String firstHalfRoom; // Stores the room where the first half of the code is displayed
 
+  /** Initializes a new instance of the RavePuzzle class. Generates a random safe code solution. */
   public RavePuzzle() {
-
     String solution = "";
-    // generate random solution
+    // Generate random solution
     Random random = new Random();
     for (int i = 0; i < 6; i++) {
       int currentNumber = random.nextInt(10);
@@ -30,28 +32,43 @@ public class RavePuzzle {
     this.safeSolution = solution; // Initialize the safe code solution
   }
 
+  /**
+   * Retrieves the generated safe code solution.
+   *
+   * @return The safe code solution as a string.
+   */
   public String getSolution() {
-    return this.safeSolution; // Retrieve the safe code solution
+    return this.safeSolution;
   }
 
+  /**
+   * Retrieves the room where the first half of the code is displayed.
+   *
+   * @return The room name as a string.
+   */
   public String getFirstHalfRoom() {
-    return this.firstHalfRoom; // Retrieve the room where the first half of the code is displayed
+    return this.firstHalfRoom;
   }
 
-  // Set hints for the puzzle in the Classical and Rock rooms
+  /**
+   * Sets hints for the puzzle in the Classical and Rock rooms by distributing the code halves.
+   *
+   * @param classical The ClassicalNoteController to set the hint in.
+   * @param rock The RockNoteController to set the hint in.
+   */
   public void setHints(ClassicalNoteController classical, RockNoteController rock) {
     System.out.println("Solution-" + safeSolution);
 
-    // split the solution into halves
+    // Split the solution into halves
     String firstHalfOfSolution = this.safeSolution.substring(0, 3);
     String secondHalfOfSolution = this.safeSolution.substring(3, 6);
 
-    // randomly select a room to have the first half of the solution
+    // Randomly select a room to have the first half of the solution
     Random random = new Random();
     int randomNum = random.nextInt(2);
 
     if (randomNum == 1) {
-      // if the first room will be in the rock room, update the note controllers for the respective
+      // If the first room will be in the rock room, update the note controllers for the respective
       // rooms
       this.firstHalfRoom = "rock";
       Platform.runLater(
@@ -60,7 +77,7 @@ public class RavePuzzle {
             rock.setText(firstHalfOfSolution);
           });
     } else {
-      // if the first room will be in the classical room, update the note controllers for the
+      // If the first room will be in the classical room, update the note controllers for the
       // respective rooms
       this.firstHalfRoom = "classical";
       Platform.runLater(
@@ -71,21 +88,27 @@ public class RavePuzzle {
     }
   }
 
-  // Set images for the puzzle in the ImageView elements
+  /**
+   * Sets images for the puzzle in the ImageView elements.
+   *
+   * @param first The first ImageView element.
+   * @param second The second ImageView element.
+   * @throws IOException If an error occurs while loading image resources.
+   */
   public void setImages(ImageView first, ImageView second) throws IOException {
-    // load the rock / classical image sprites
+    // Load the rock / classical image sprites
     Image rock = new Image(App.class.getResource("/images/misc/rockicon.png").openStream());
     Image classical =
         new Image(App.class.getResource("/images/misc/classicalicon.png").openStream());
     Platform.runLater(
-        // update the gui for each image
+        // Update the GUI for each image
         () -> {
           if (this.firstHalfRoom.equals("rock")) {
-            // if the first half is rock, the first image should be the rock sprite
+            // If the first half is in the rock room, the first image should be the rock sprite
             first.setImage(rock);
             second.setImage(classical);
           } else {
-            // otherwise the order should be reversed
+            // Otherwise, the order should be reversed
             first.setImage(classical);
             second.setImage(rock);
           }
