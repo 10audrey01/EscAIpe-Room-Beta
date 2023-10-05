3014a16e790eabc02d064c37d897e1c45b7f7d08
Liyork Liao <liyorkl@gmail.com>
2023-10-05 15:44:24 +1300

    Refactored - Obejective list manager reduced the use of multiple arraylist by making arraylist of arraylists changed the way you add labels and keys to look smoother reducing the amount of repeated functions

--- src/main/java/nz/ac/auckland/se206/ObjectiveListManager.java
+++ src/main/java/nz/ac/auckland/se206/ObjectiveListManager.java
@@ -7,71 +7,45 @@ import javafx.scene.text.Text;
 
 public class ObjectiveListManager {
 
-  private ArrayList<Label> objectiveLabel1;
-  private ArrayList<Label> objectiveLabel2;
-  private ArrayList<Label> objectiveLabel3;
-  private ArrayList<Label> objectiveLabel4;
-  private ArrayList<ImageView> step1Key;
-  private ArrayList<ImageView> step2Key;
-  private ArrayList<ImageView> step3Key;
-  private ArrayList<ImageView> step4Key;
+  private ArrayList<ArrayList<Label>> allObjectiveLabels;
+  private ArrayList<ArrayList<ImageView>> allStepKeys;
 
   public ObjectiveListManager() {
     // Initialize lists for objective labels and step keys
-    this.objectiveLabel1 = new ArrayList<Label>();
-    this.objectiveLabel2 = new ArrayList<Label>();
-    this.objectiveLabel3 = new ArrayList<Label>();
-    this.objectiveLabel4 = new ArrayList<Label>();
-    this.step1Key = new ArrayList<ImageView>();
-    this.step2Key = new ArrayList<ImageView>();
-    this.step3Key = new ArrayList<ImageView>();
-    this.step4Key = new ArrayList<ImageView>();
+    this.allObjectiveLabels = new ArrayList<ArrayList<Label>>();
+    this.allStepKeys = new ArrayList<ArrayList<ImageView>>();
   }
 
-  // Add an objective label for step 1
-  public void addObjectiveLabel1(Label objectiveLabel) {
-    this.objectiveLabel1.add(objectiveLabel);
+  // Add an objective label for any step
+  public void addObjectiveLabel(int labelIndex, Label objectiveLabel) {
+    this.allObjectiveLabels.get(labelIndex).add(objectiveLabel);
   }
 
-  // Add an objective label for step 2
-  public void addObjectiveLabel2(Label objectiveLabel) {
-    this.objectiveLabel2.add(objectiveLabel);
+  // Add an objective labels for all steps
+  public void addObjectiveLabels(ArrayList<Label> objectiveLabels) {
+    this.addObjectiveLabel(0, objectiveLabels.get(0));
+    this.addObjectiveLabel(1, objectiveLabels.get(1));
+    this.addObjectiveLabel(2, objectiveLabels.get(2));
+    this.addObjectiveLabel(3, objectiveLabels.get(3));
   }
 
-  // Add an objective label for step 3
-  public void addObjectiveLabel3(Label objectiveLabel) {
-    this.objectiveLabel3.add(objectiveLabel);
+  // Add an ImageView representing a key for any step
+  public void addStepKey(int keyIndex, ImageView stepKeys) {
+    this.allStepKeys.get(keyIndex).add(stepKeys);
   }
 
-  // Add an objective label for step 4
-  public void addObjectiveLabel4(Label objectiveLabel) {
-    this.objectiveLabel4.add(objectiveLabel);
-  }
-
-  // Add an ImageView representing a key for step 1
-  public void addStep1Key(ImageView step1Key) {
-    this.step1Key.add(step1Key);
-  }
-
-  // Add an ImageView representing a key for step 2
-  public void addStep2Key(ImageView step2Key) {
-    this.step2Key.add(step2Key);
-  }
-
-  // Add an ImageView representing a key for step 3
-  public void addStep3Key(ImageView step3Key) {
-    this.step3Key.add(step3Key);
-  }
-
-  // Add an ImageView representing a key for step 4
-  public void addStep4Key(ImageView step4Key) {
-    this.step4Key.add(step4Key);
+  // Add an ImageView representing a key for all steps
+  public void addStepKeys(ArrayList<ImageView> stepKeys) {
+    this.addStepKey(0, stepKeys.get(0));
+    this.addStepKey(1, stepKeys.get(1));
+    this.addStepKey(2, stepKeys.get(2));
+    this.addStepKey(3, stepKeys.get(3));
   }
 
   // Mark objective 1 as completed if the music quiz is completed
   public void completeObjective1() {
     if (GameState.isMusicQuizCompleted) {
-      strikeThroughLabels(this.objectiveLabel1); // Apply strikethrough effect to labels
+      strikeThroughLabels(this.allObjectiveLabels.get(0)); // Apply strikethrough effect to labels
       setVisibilityKeyBlue1(true); // Set visibility of step 1 key to true
     }
   }
@@ -79,7 +53,7 @@ public class ObjectiveListManager {
   // Mark objective 2 as completed if the safe is opened
   public void completeObjective2() {
     if (GameState.isSafeOpened) {
-      strikeThroughLabels(this.objectiveLabel2); // Apply strikethrough effect to labels
+      strikeThroughLabels(this.allObjectiveLabels.get(1)); // Apply strikethrough effect to labels
       setVisibilityKeyGreen2(true); // Set visibility of step 2 key to true
     }
   }
@@ -87,14 +61,14 @@ public class ObjectiveListManager {
   // Mark objective 3 as completed if the riddle is resolved
   public void completeObjective3() {
     if (GameState.isRiddleResolved) {
-      strikeThroughLabels(this.objectiveLabel3); // Apply strikethrough effect to labels
+      strikeThroughLabels(this.allObjectiveLabels.get(2)); // Apply strikethrough effect to labels
     }
   }
 
   // Mark objective 4 as completed if the harp is played
   public void completeObjective4() {
     if (GameState.isHarpPlayed) {
-      strikeThroughLabels(this.objectiveLabel4); // Apply strikethrough effect to labels
+      strikeThroughLabels(this.allObjectiveLabels.get(3)); // Apply strikethrough effect to labels
       setVisibilityKeyYellow4(true); // Set visibility of step 4 key to true
     }
   }
@@ -115,28 +89,28 @@ public class ObjectiveListManager {
 
   // Set visibility of step 1 key ImageView(s)
   public void setVisibilityKeyBlue1(boolean visibility) {
-    for (ImageView key : step1Key) {
+    for (ImageView key : allStepKeys.get(0)) {
       key.setVisible(visibility);
     }
   }
 
   // Set visibility of step 2 key ImageView(s)
   public void setVisibilityKeyGreen2(boolean visibility) {
-    for (ImageView key : step2Key) {
+    for (ImageView key : allStepKeys.get(1)) {
       key.setVisible(visibility);
     }
   }
 
   // Set visibility of step 3 key ImageView(s)
   public void setVisibilityKeyRed3(boolean visibility) {
-    for (ImageView key : step3Key) {
+    for (ImageView key : allStepKeys.get(2)) {
       key.setVisible(visibility);
     }
   }
 
   // Set visibility of step 4 key ImageView(s)
   public void setVisibilityKeyYellow4(boolean visibility) {
-    for (ImageView key : step4Key) {
+    for (ImageView key : allStepKeys.get(3)) {
       key.setVisible(visibility);
     }
   }
