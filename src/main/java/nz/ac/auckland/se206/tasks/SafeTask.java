e498111d8dd4338b49d7af6f9d86007faf489040
audrey <ayou332@aucklanduni.ac.nz>
2023-10-16 19:31:57 +1300

    feat - hints given based on what is shown on objective list, will not give hints for tasks no on list and added trumpet, bodybuilder and dj hint

--- src/main/java/nz/ac/auckland/se206/tasks/SafeTask.java
+++ src/main/java/nz/ac/auckland/se206/tasks/SafeTask.java
@@ -3,7 +3,7 @@ package nz.ac.auckland.se206.tasks;
 public class SafeTask extends Task {
   public SafeTask() {
     super();
-    this.taskDescription = "- Help the bodybulder";
+    this.taskDescription = "- Help the bodybuilder";
     this.isCompleted = false;
     this.hasMoreTasks = false;
   }
